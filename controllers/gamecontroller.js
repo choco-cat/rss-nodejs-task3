916cc91a8629dcbfa512edb6981a10993d28a980
Panfilenka Natallia <choco-cat@mail.ru>
2021-05-20 14:39:42 +0300

    fix: logical errors

--- controllers/gamecontroller.js
+++ controllers/gamecontroller.js
@@ -6,7 +6,7 @@ const modelGame = require("../models/game");
 const Game = modelGame(sequelize, DataTypes);
 
 router.get("/all", (req, res) => {
-    Game.findAll({ where: { owner_id: req.body.user.id } })
+    Game.findAll({ where: { owner_id: req.user.id } })
         .then(
             (games) => {
                 res.status(200).json({
@@ -24,7 +24,7 @@ router.get("/all", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-    Game.findOne({ where: { id: req.params.id, owner_id: req.body.user.id } })
+    Game.findOne({ where: { id: req.params.id, owner_id: req.user.id } })
         .then(
             (game) => {
                 res.status(200).json({
@@ -43,7 +43,7 @@ router.get("/:id", (req, res) => {
 router.post("/create", (req, res) => {
     Game.create({
         title: req.body.game.title,
-        owner_id: req.body.user.id,
+        owner_id: req.user.id,
         studio: req.body.game.studio,
         esrb_rating: req.body.game.esrb_rating,
         user_rating: req.body.game.user_rating,
@@ -74,7 +74,7 @@ router.put("/update/:id", (req, res) => {
         {
             where: {
                 id: req.params.id,
-                owner_id: req.body.user.id
+                owner_id: req.user.id
             }
         })
         .then(
@@ -98,7 +98,7 @@ router.delete("/remove/:id", (req, res) => {
     Game.destroy({
         where: {
             id: req.params.id,
-            owner_id: req.body.user.id
+            owner_id: req.user.id
         }
     })
     .then(
